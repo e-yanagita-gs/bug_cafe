5f91b3bebfa6d9fc932a72e7e41206412c8def73
e-yanagita-gs <e.yanagita@genbasupport.com>
2024-11-08 14:11:20 +0900

    Re-Revised version

--- cafe.rb
+++ cafe.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-#require 'debug'
 
 DRINKS = [
   { name: 'コーヒー', price: '300' },
@@ -25,14 +24,11 @@ def take_order(menus)
   order_number
 end
 
-
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order1 = take_order(DRINKS)
+order_drink = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
+order_food = take_order(FOODS)
 
-#binding.break
-
-total = DRINKS[order1][:price].to_i + FOODS[order2][:price].to_i
+total = DRINKS[order_drink][:price].to_i + FOODS[order_food][:price].to_i
 puts "お会計は#{total}円になります。ありがとうございました！"
